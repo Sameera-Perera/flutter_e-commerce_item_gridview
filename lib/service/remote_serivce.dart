5afd71eeb0e1169708e317a6045f49813868c35e
Sameera Perera <49270152+Sameera-Perera@users.noreply.github.com>
2022-12-11 15:23:12 +0530

    Update remote_serivce.dart
    update new server

--- lib/service/remote_serivce.dart
+++ lib/service/remote_serivce.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 class RemoteServices {
   static var client = http.Client();
-  static var baseURL = 'https://my-grocery-strapi.herokuapp.com';
+  static var baseURL = 'https://grocery-app-sc6n.onrender.com';
 
   static Future<dynamic> fetchItem(int start) async {
     var response = await client.get(
